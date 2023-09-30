68179d59ad8aee48cfe2e63017ff221c2162d3a9
Phelipe Lani <p.lani@live.com>
2023-09-30 15:16:20 -0300

    fix: back

--- Back/rodadas/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-from ligas.models import Liga
-
-class Rodada(models.Model):
-    liga = models.ForeignKey(Liga, on_delete=models.CASCADE)  
-    data_da_rodada = models.DateField()
--- Back/users/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import UserView
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-
-urlpatterns = [
-    path("users/", UserView.as_view()),
-    path("login/", TokenObtainPairView.as_view()),
-    path("refresh/", TokenRefreshView.as_view()),
-]
