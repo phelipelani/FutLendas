2f3ae74c188d89c2460024e9fd49541fa1e5ee0a
Phelipe Lani <p.lani@live.com>
2023-10-09 08:13:18 -0300

    postgress

--- Back/FutLendas/settings.py
+++ Back/FutLendas/settings.py
@@ -83,8 +83,12 @@ WSGI_APPLICATION = "FutLendas.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "Cartolendas",
+        "USER": "postgres",
+        "PASSWORD": "160416Dud@",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
